80cc6530eee51033433b3e4101f645575281a437
Satyam Bharti <satyam.b@genisys-group.com>
2022-03-16 19:41:28 +0530

    delet

--- programm.c
+++ programm.c
@@ -1,107 +1,105 @@
 
+
 #include<stdio.h>
 #include<stdlib.h>
 
+void insert(int );
+void display_List();
+void delete(int );
 
-struct Node
+struct node			 
 {
 	int data;
-	struct Node *next;
-};
-
-void insertAtTheBegin(struct Node **start_ref, int data);
+	struct node *next; 
+}*head=NULL,*tail=NULL; 
 
 
-void bubbleSort(struct Node *start);
+void delete(int pos)
+{
+	struct node *temp = head;	 
+	int i;					
+	if(pos==0)
+	{
+		printf("\nElement deleted is : %d\n",temp->data);
+		head=head->next;	 
+		temp->next=NULL;
+		free(temp);			 
+	}
+	else
+	{
+		for(i=0;i<pos-1;i++)
+		{
+			temp=temp->next;
+		}
+		
+		struct node *del =temp->next;	 
+		temp->next=temp->next->next;
+		printf("\nElement deleted is : %d\n",del->data);	
+		del->next=NULL;
+		free(del);						 
+	}
+	printf("\nUpdated Linked List is : \n");
+	display_List();
+	return ;
+}
 
 
-void swap(struct Node *a, struct Node *b);
 
+void insert(int value)
+{
+	struct node *newnode; 
+	newnode = (struct node *)malloc(sizeof(struct node)); 
 
-void printList(struct Node *start);
+	newnode->data = value;	 
+	newnode->next = NULL;	
+
+	if(head==NULL)	 
+	{
+		head = newnode;
+		tail = newnode;
+	}
+	else			 
+	{
+		tail->next=newnode;	
+		tail=newnode;
+	}
+	return ;
+}
+
+void display_List()
+{
+	struct node *temp; 
+	temp=head;		 
+	while(temp!=NULL)
+	{
+		if(temp->next==NULL)
+		{
+			printf(" %d->NULL",temp->data);
+		}
+		else
+		{
+			printf(" %d->",temp->data);
+		}
+		temp=temp->next;		 
+	}
+	printf("\n");
+	return ;
+}
 
 int main()
 {
-	int arr[] = {12, 56, 2, 11, 1, 90};
-	int list_size, i;
-
-	
-	struct Node *start = NULL;
-
-	
-	for (i = 0; i< 6; i++)
-		insertAtTheBegin(&start, arr[i]);
-
-	
-	printf("\n Linked list before sorting ");
-	printList(start);
-
-	
-	bubbleSort(start);
-
-	
-	printf("\n Linked list after sorting ");
-	printList(start);
-
-	getchar();
+	insert(10);
+	insert(20);
+	insert(30);
+	insert(40);
+	insert(50);
+	insert(60);
+	printf("\n--Created Linked List--\n");
+	display_List();
+	printf("\nLinked List after deletion at position 0");
+	delete(0);								 
+	printf("\nLinked List after deletion at position 2");
+	delete(2);
 	return 0;
 }
 
-
-
-void insertAtTheBegin(struct Node **start_ref, int data)
-{
-	struct Node *ptr1 = (struct Node*)malloc(sizeof(struct Node));
-	ptr1->data = data;
-	ptr1->next = *start_ref;
-	*start_ref = ptr1;
-}
-
-
-void printList(struct Node *start)
-{
-	struct Node *temp = start;
-	printf("\n");
-	while (temp!=NULL)
-	{
-		printf("%d ", temp->data);
-		temp = temp->next;
-	}
-}
-
-void bubbleSort(struct Node *start)
-{
-	int swapped, i;
-	struct Node *ptr1;
-	struct Node *lptr = NULL;
-
-	
-	if (start == NULL)
-		return;
-
-	do
-	{
-		swapped = 0;
-		ptr1 = start;
-
-		while (ptr1->next != lptr)
-		{
-			if (ptr1->data > ptr1->next->data)
-			{
-				swap(ptr1, ptr1->next);
-				swapped = 1;
-			}
-			ptr1 = ptr1->next;
-		}
-		lptr = ptr1;
-	}
-	while (swapped);
-}
-
-
-void swap(struct Node *a, struct Node *b)
-{
-	int temp = a->data;
-	a->data = b->data;
-	b->data = temp;
-}
